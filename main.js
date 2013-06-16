5d17314fdecb0110f5db1fa397a8a2ecf64fb952
Endo <endo4917@goo.jp>
2013-06-16 14:25:11 +0900

    jquery-ui added

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 $(function(){
   ws = new WebSocket("ws://localhost:5123");
   ws.onmessage = function(evt){
-    $("#msg").append(evt.data+"<br />");
-  };
-  ws.onclose = function(){
-    console.log("finished");
-  };
-  ws.onopen = function(){
-    ws.send($("#name").val()+"が参加しました。");
+    $("#msg").append("<p>"+evt.data+"</p> ");
+    $("#msg").scrollTop($("#msg")[0].scrollHeight);
+    $("p:last").css({'backgroundColor': 'yellow'}).
+    animate({'backgroundColor': 'white'}, 1000);
   };
   $("#input").keypress(function(e){
     if(e.keyCode==13){
-      var str = $("#name").val()+":"+$("#input").val();
+      var str = $("#name").val()+": "+$("#input").val();
       ws.send(str);
       $("#input").val("");
     };
